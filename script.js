7eaef7ac1f7db783c3c86c4691381605ec97a03f
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-09-26 20:30:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
 console.warn('no data to fetch');
+window.alert(`Hello! You're using acuthe weather, an app that's still in development.\nPlease don't block our app from displaying these, to know when the app is finished.`);
