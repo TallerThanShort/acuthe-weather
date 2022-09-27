223ddde9519ea84bb13a2020ad6b57318bcdb805
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-09-27 20:19:16 +0200

    uhh no more react

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
+//imports
+let alertwrap = select("#modal-alerter");
+
+//some demo shid
 console.warn('no data to fetch');
-window.alert(`Hello! You're using acuthe weather, an app that's still in development.\nPlease don't block our app from displaying these, to know when the app is finished.`);
+alertwrap.style.display = `block`;
+
+function okAlert() {
+    alertwrap.style.display = `none`;
+}
+
+//simple function to write this shid faster
+function select(el) {
+    return document.querySelector(el);
+}
+
+//FDOM activate
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+  
+});
